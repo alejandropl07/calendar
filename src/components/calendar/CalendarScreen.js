dbf9630982bbbbe53a0e33e4145b543b2de86f44
alejandropl07 <aprietoleon97@gmail.com>
2022-07-18 11:11:28 -0500

    react-big-calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import Navbar from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "My birthday",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    notes: "Party",
+  },
+];
 
 const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      color: "white",
+      display: "block",
+      opacity: 0.8,
+    };
+    return { style };
+  };
+
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          eventPropGetter={eventStyleGetter}
+        />
+      </div>
     </div>
   );
 };
